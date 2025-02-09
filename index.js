7fea7fd422a86c659ee36500f892d82731551820
puliganti mahendar <puligantimahi@gmail.com>
2025-02-09 16:32:07 +0530

    correct port

--- index.js
+++ index.js
@@ -188,6 +188,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { err });
 });
 
-app.listen(3000, () => {
-  console.log("app runnig at 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`the app is running at ${port}`);
 });
